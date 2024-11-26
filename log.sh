9c96dbedd849ca22e3f5ab5ec7eb612990dc0e9b
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-26 17:57:53 +0100

    Update log.sh

--- log.sh
+++ log.sh
@@ -3,4 +3,4 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-journalctl -n 100 -f $folder.service --no-pager
+journalctl -n 200 -u $folder.service -f --no-hostname -o cat
