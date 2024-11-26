2637a8594a7c2462202c9d911456912efea051cd
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-26 17:57:01 +0100

    Create update-scripts.sh

---
+++ update-scripts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+cd ~/scripts/$folder
+git stash push --include-untracked
+git pull
+chmod +x *.sh
