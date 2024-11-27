31704119cb5507477ab9a943b75cf0abe25d1076
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-27 10:22:26 +0100

    Update log-err.sh

--- log-err.sh
+++ log-err.sh
@@ -3,4 +3,4 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-journalctl -u $folder.service --no-hostname -o cat | grep ERROR
+journalctl -u $folder.service --no-hostname -o cat | grep -E "ERR|rror"
