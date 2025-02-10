5927d39b6f7105391e4a7792709b71bdb7a9e060
Motko <77466238+Motko222@users.noreply.github.com>
2025-02-10 23:19:43 +0100

    Update ref-signup.sh

--- ref-signup.sh
+++ ref-signup.sh
@@ -3,7 +3,7 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-read -p "Referral code?" ref
+read -p "Referral code? " ref
 
 cd /root/$folder
 ./pop --signup-by-referral-route $ref
