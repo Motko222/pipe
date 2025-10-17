e619b048fd2d00de1caff265cd96ac54a8c40d2f
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-17 22:18:43 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -12,7 +12,8 @@ rm /etc/systemd/system/$folder.service
 
 mkdir /root/backup/$folder" folder"
 cp /root/$folder/node_info.json /root/backup/$folder" folder"
-#rm -r /root/$folder
+rm -r /root/$folder
 mv /root/scripts/$folder /root/backup
+rm -r /root/backup/$folder/.git
 bash /root/scripts/system/influx-delete-id.sh $folder
 
